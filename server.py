8a5448c3abbcae1e853b70a760eb1c97d5d35a05
kongziheng <152956525+kongziheng@users.noreply.github.com>
2025-06-15 20:33:01 +0800

    Processing OK Responses and Creating Dedicated Data Ports

--- server.py
+++ server.py
@@ -12,3 +12,9 @@ def handle_client(addr, filename, server_port):
             err_msg = f"ERR {filename} NOT_FOUND"
             server.sendto(err_msg.encode(), addr)
             return
+        filesize = os.path.getsize(filepath)
+        transfer_port = random.randint(50000, 51000)
+        server.sendto(f"OK {filename} SIZE {filesize} PORT {transfer_port}".encode(), addr)
+
+        data_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        data_socket.bind(('', transfer_port))
